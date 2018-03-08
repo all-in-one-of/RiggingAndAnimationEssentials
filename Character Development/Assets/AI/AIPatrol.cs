f23b35705213c0b3204b12c51ec7f2b82692014c
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2018-03-08 14:51:45 -0700

    Added AI w/ SO

---
+++ Character Development/Assets/AI/AIPatrol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Patrol")]
+
+public class AIPatrol : AIBase
+{
+    private int i = 0;
+    
+    public Transform[] PatrolPoints;
+
+    public override void Navigate(UnityEngine.AI.NavMeshAgent ai)
+    {
+        if (ai.remainingDistance < 1)
+        {
+            ChangePatrolPoint();
+        }
+
+        ai.destination = PatrolPoints[i].position;
+    }
+
+    private void ChangePatrolPoint()
+    {
+        if (i < PatrolPoints.Length - 1)
+        {
+            i++;
+        }
+        else
+        {
+            i = 0;
+        }
+    }
+}
---
+++ Character Development/Assets/AI/NavMeshBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshBehaviour : MonoBehaviour
+{
+	private NavMeshAgent NavMeshAgent;
+
+	public AIBase AIBase;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		NavMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		AIBase.Navigate(NavMeshAgent);
+	}
+}
